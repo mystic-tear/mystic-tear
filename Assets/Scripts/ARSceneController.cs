65acc005c77a7eeec53f7d87c3e18b888f53c310
Roger V <60841571+rogerrogerv@users.noreply.github.com>
2020-06-09 12:48:13 +0900

    commit in order to pull
    Co-authored-by: Julia Gross <sakurtoshi@gmail.com>
    Co-authored-by: Kohki Shiga <saggitarie@gmail.com>
    Co-authored-by: Halee Pagel <halee.pagel@gmail.com>

---
+++ Assets/Scripts/ARSceneController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class ARSceneController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void QuitOnConnectionErrors()
+    {
+        if (Session.Status ==  SessionStatus.ErrorPermissionNotGranted)
+        {
+            StartCoroutine(CodelabUtils.ToastAndExit(
+              "Camera permission is needed to run this application.", 5));
+        }
+        else if (Session.Status.IsError())
+        {
+        // This covers a variety of errors.  See reference for details
+        // https://developers.google.com/ar/reference/unity/namespace/GoogleARCore
+        StartCoroutine(CodelabUtils.ToastAndExit(
+            "ARCore encountered a problem connecting. Please restart the app.", 5));
+        }
+    }
+}
