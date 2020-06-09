ff8799bd62ce73f3ea5fd8f7c425c9f8eb4869c9
Roger V <60841571+rogerrogerv@users.noreply.github.com>
2020-06-09 12:48:13 +0900

    commit in order to pull
    Co-authored-by: Julia Gross <sakurtoshi@gmail.com>
    Co-authored-by: Kohki Shiga <saggitarie@gmail.com>
    Co-authored-by: Halee Pagel <halee.pagel@gmail.com>

---
+++ Assets/Demo/ARTapToPlaceObject2.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.Experimental.XR;
+using System;
+
+public class ARTapToPlaceObject2 : MonoBehaviour
+{
+    public GameObject objectToPlace;
+    public GameObject placementIndicator;
+    private ARSessionOrigin arOrigin;
+    private Pose PlacementPose;
+    private bool placementPoseIsValid = false;
+    void Start()
+    {
+    arOrigin = FindObjectOfType<ARSessionOrigin>();
+    }
+     
+    void Update()
+    {
+        UpdatePlacementPose();
+        UpdatePlacementIndicator();
+
+        if (placementPoseIsValid && Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            PlaceObject();
+        }
+    }
+
+    private void PlaceObject()
+    {
+        Instantiate(objectToPlace, PlacementPose.position, PlacementPose.rotation);
+    }
+
+    private void UpdatePlacementIndicator()
+    {
+
+        if (placementPoseIsValid)
+        {
+            placementIndicator.SetActive(true);
+            placementIndicator.transform.SetPositionAndRotation(PlacementPose.position, PlacementPose.rotation);
+        }
+        else
+        {
+            placementIndicator.SetActive(false);
+        }
+    }
+
+    private void UpdatePlacementPose()
+    {
+        var screenCenter = Camera.current.ViewportToScreenPoint(new Vector3(0.5f, 0.5f));
+        var hits = new List<ARRaycastHit>();
+        arOrigin.GetComponent<ARRaycastManager>().Raycast(screenCenter, hits, UnityEngine.XR.ARSubsystems.TrackableType.Planes);
+
+
+        placementPoseIsValid = hits.Count > 0;
+
+        if (placementPoseIsValid)
+        {
+            PlacementPose = hits[0].pose;
+            var cameraForward = Camera.current.transform.forward;
+            var cameraBearing = new Vector3(cameraForward.x, 0, cameraForward.z).normalized;
+            PlacementPose.rotation = Quaternion.LookRotation(cameraBearing);
+        }
+
+    }
+}
