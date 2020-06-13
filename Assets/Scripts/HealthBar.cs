fb84303861ced95eb1c83adc3677c8b6615a8407
Saggitarie <saggitarie@gmail.com>
2020-06-14 03:19:42 +0900

    MysticTear Boilerplate for mac users

---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    public Slider healthSlider;
+    public void SetMaxHealth(int maxHealth){
+        healthSlider.maxValue = maxHealth;
+        healthSlider.value = maxHealth;
+    }
+
+    public void SetHealth(int health){
+        healthSlider.value = health;
+    }
+}
