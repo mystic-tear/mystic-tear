7325abf719c2cecffdddffde7f5f962fd053adc7
Roger V <60841571+rogerrogerv@users.noreply.github.com>
2020-06-16 03:25:33 +0900

    Start the process of adding battle capabilities

---
+++ Assets/Scripts/CreatureDataObject.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewDataObject", menuName = "Scriptables/NewDataObj", order = 1)]
+public class CreatureHealthVariable : ScriptableObject
+{
+    
+}
---
+++ Assets/Scripts/EnemyFollow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFollow : MonoBehaviour
+{
+    public float speed = 5;
+    public float stoppingDistance = 3;
+
+    private Transform target;
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("good").GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+        if(Vector2.Distance(transform.position, target.position) > stoppingDistance)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        }
+    }
+}
